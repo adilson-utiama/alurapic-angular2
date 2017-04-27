755963a8997ca5f0a5b9ba773afc3c5a1aa869ce
Adilson <asu2u@yahoo.com.br>
2017-04-26 23:27:50 -0300

    Http e injecao de dependencias

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html'
 })
 
-export class AppComponent {}
+export class AppComponent {
+
+    //Poderiamos usar o decorator @Inject(Http) http neste caso
+    constructor(http: Http) {
+
+    }
+}
