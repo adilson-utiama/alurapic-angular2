9104186a9c70fd3c1b738d3b389869d6c1b7c8df
Adilson <asu2u@yahoo.com.br>
2017-04-28 22:24:52 -0300

    Formulario de cadastro, two-way-databinding e event binding

--- app/cadastro/cadastro.component.ts
+++ app/cadastro/cadastro.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
 
 @Component({
     moduleId: module.id,
     selector: 'cadastro',
     templateUrl: './cadastro.component.html'
 })
-export class CadastroComponent {}
+export class CadastroComponent {
+
+    foto: FotoComponent = new FotoComponent();
+
+    constructor() {
+        this.foto.titulo = '';
+        this.foto.url = '';
+        this.foto.descricao = '';
+    }
+
+    cadastrar(event){
+        event.preventDefault();
+
+        console.log(this.foto);
+    }
+}
